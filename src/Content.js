01c87b3093fa008c81cb743cff992100c9d28ed6
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-04-19 17:25:05 -0800

    Feat: Added HTML injected description content

--- src/Content.js
+++ src/Content.js
@@ -3,11 +3,15 @@ import Row from "muicss/lib/react/row";
 import Col from "muicss/lib/react/col";
 
 import EventForm from "./EventForm";
+import dummyContent from "./Demo/dummyData";
 
 const Content = () => (
   <Row>
     <Col md="12">
-      <EventForm title="Tango Beginner Workshop" />
+      <EventForm
+        title={dummyContent.title}
+        description={dummyContent.content}
+      />
     </Col>
   </Row>
 );
--- src/EventForm.js
+++ src/EventForm.js
@@ -4,12 +4,13 @@ import Radio from "muicss/lib/react/radio";
 import Button from "muicss/lib/react/button";
 import Input from "muicss/lib/react/input";
 import Panel from "muicss/lib/react/panel";
+import Col from "muicss/lib/react/col";
 
 class EventForm extends Component {
   constructor(props) {
     super(props);
-    const { title } = props;
-    this.state = { value: "", form: { title } };
+    const { title, description } = props;
+    this.state = { value: "", form: { title, description } };
   }
 
   handleChange(event) {
@@ -28,23 +29,31 @@ class EventForm extends Component {
           <h1 className="App-title">{this.state.form.title}</h1>
         </header>
         <Panel>
-          <form onSubmit={() => this.handleSubmit}>
-            <legend>Description</legend>
-            <div>{this.state.form.description}</div>
-            <Radio name="register" label="Lead" />
-            <Radio name="register" label="Follow" />
-            <Radio name="register" label="Both" />
+          <Form onSubmit={() => this.handleSubmit}>
+            <br />
+            <Col md="8">
+              <legend>Description</legend>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: this.state.form.description
+                }}
+              />
+            </Col>
+            <Col md="4">
+              <Radio name="register" label="Lead" />
+              <Radio name="register" label="Follow" />
+              <Radio name="register" label="Both" />
 
-            <Input
-              label="Email Address"
-              type="email"
-              defaultValue="Validation error"
-            />
-
+              <Input
+                label="Email Address"
+                type="email"
+                defaultValue="Validation error"
+              />
+            </Col>
             <Button variant="raised" type="submit">
               Submit
             </Button>
-          </form>
+          </Form>
         </Panel>
       </div>
     );
