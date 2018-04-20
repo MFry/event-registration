6143ee8d527f0d02de47219757c8d8a828415f6d
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-04-19 17:23:02 -0800

    Feat: Added demo data for sample event

---
+++ src/Demo/dummyData.js
@@ -0,0 +1,49 @@
+const title = `All Levels and Intermediate Tango Jump Start w/ Emily Webb`;
+const content = `
+Are you ready to solidify intermediate level and start upping your dance game?<br />
+<br />
+Expand your moves, Discover the secrets of great connection, and unravel the mystery of how to listen and dance to Tango music.<br />
+<br />
+Class 1: Your Tango Workout: Excersices and drills to give you a rock solid axis and create fluidity in your movements and connection.<br /> 
+All Levels<br />
+1:15-2:15<br />
+<br />
+Class 2: Understanding and applying Tango Musicality: Make even your simplest moves fun and creative by letting the music move you. <br />
+All Levels<br />
+2:25-3:25<br />
+<br />
+Class 3: Intro to Boleos: High and Low, Forward and Back, With and Contra. Explore all the variations and the technique of leg flying fun! <br />
+Intermediate<br />
+3:35-4:35 <br />
+<br />
+Class 4: Volcada Beginnings: Learn how to effectively share weight and counter balance your partners in a series of fantasically fun moves! <br />
+Intermediate<br />
+4:45-5:45<br />
+<br />
+Register Now for early bird pricing!<br />
+Early Bird- $45 (while tickets last)<br />
+Regular Price - $50<br />
+Day of Event - $60/4 classes or $15 per class<br />
+Individual Class $15 <br r/>
+2 or 3 classes $12.50 each<br />
+<br />
+For more information contact Emily - <br />
+TangoandFusion@gmail.com <br />
+<br />
+Here is what some people have to say about Emily's workshops:<br />
+<br />
+This was one of the best dance workshops I have been too. Great followers. Great instructors.<br />
+Mark Susol, Cupertino, CA<br />
+<br />
+I've often felt intimidated by the slow learning curve I experience with tango. Emily’s classes give me enough information to jump in while still developing my technique + proper mechanics. I feel so much more confident in my tango and it’s been a really fun embellishment when I dance fusion. <br />
+<br />
+Emily is very knowledgeable and is able to convey both the leader and follower's steps. She guides you through each sequence, and allows you to connect with your dance partner naturally. I would take another one of her classes in a heartbeat!<br />
+<br />
+More about Emily:<br />
+In 1996 Emily Webb began her love affair with partner dance. Through the years Emily has studied Tango, Swing, Salsa, Blues, and Fusion dance with some the top instructors in the world. Today Tango and Fusion remain Emily's greatest dance loves. <br />
+<br />
+Thousands of students across the US and Canada have benefited from Emily's dynamic and interactive teaching style. Emily's adaptability to student needs, knowledge of body mechanics, and specialized drills create high impact learning for students of all levels. Through her years of teaching experience Emily has learned to make even the most complex movements simple and build them from the ground up. This means you will not only look good while you dance, but feel great to your partners too! <br />
+<br />
+Emily has completed multiple teachers trainings in and out of dance, is a certified instructor of Krav Maga (martial art) and has a BS in Equine Science with a specialty in teaching. She has been seen teaching at nationally recognized events such as the Portland Alt Tango Festival, Denver Fusion Exchange , Las Vegas Fusion Exchange, Atomic Fusion Festival, Massive Blues Recess, Seattle Fusion Festival, Mission Fusion Extravaganza, DJX Fusion Weekend, The Fusion Project and more.<br />
+`;
+export default { title, content };
