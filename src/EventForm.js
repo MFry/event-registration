ced22b08577b0aa767094605061379531cae1715
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-04-26 17:07:24 -0800

    Feat: SVG material icons, toolkits and open spots
    Added UI elements to display number of available open spots
    Added future sharing option

--- src/EventForm.js
+++ src/EventForm.js
@@ -11,6 +11,9 @@ import Input, { InputLabel } from "material-ui/Input";
 import { MenuItem } from "material-ui/Menu";
 import { FormControl, FormHelperText } from "material-ui/Form";
 import Select from "material-ui/Select";
+import SvgIcon from "material-ui/SvgIcon";
+import Tooltip from "material-ui/Tooltip";
+import blue from "material-ui/colors/blue";
 import Moment from "react-moment";
 import "./material-form.css";
 
@@ -48,6 +51,13 @@ const styles = theme => ({
     top: 0,
     zIndex: 9999
   },
+  menuIcons: {
+    fontSize: "2rem",
+    cursor: "pointer",
+    "&:hover": {
+      color: blue[400]
+    }
+  },
   footer: {
     height: "10vh"
   }
@@ -56,7 +66,15 @@ const styles = theme => ({
 class EventForm extends Component {
   constructor(props) {
     super(props);
-    const { title, description, startDateTime, endDateTime, address } = props;
+    const {
+      title,
+      description,
+      startDateTime,
+      endDateTime,
+      address,
+      openSlots,
+      maxSlots
+    } = props;
     this.state = {
       value: "",
       roleValue: "",
@@ -64,7 +82,9 @@ class EventForm extends Component {
         title,
         description,
         date: { start: startDateTime, end: endDateTime },
-        address
+        address,
+        openSlots,
+        maxSlots
       }
     };
   }
@@ -80,6 +100,16 @@ class EventForm extends Component {
 
   render() {
     const dash = <span>—</span>;
+    const ShareSvgIcon = props => (
+      <SvgIcon {...props}>
+        <path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z" />
+      </SvgIcon>
+    );
+    const GroupSvgIcon = props => (
+      <SvgIcon {...props}>
+        <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z" />
+      </SvgIcon>
+    );
     const { classes } = this.props;
     return (
       <form className={classes.root} onSubmit={() => this.handleSubmit}>
@@ -93,7 +123,36 @@ class EventForm extends Component {
 
         <Grid container className={classes.stickyHeader}>
           <Grid item xs={12} className={classes.formSpacing} />
-          <Grid item xs={8} className={classes.content} />
+          <Grid item xs={1} className={classes.content}>
+            <Grid container style={{ alignItems: "center", height: "100%" }}>
+              <Tooltip title="Share this event" placement="bottom">
+                <ShareSvgIcon
+                  className={classes.menuIcons}
+                  onClick={e => {
+                    console.log(e.target);
+                  }}
+                />
+              </Tooltip>
+            </Grid>
+          </Grid>
+          <Grid item xs={2} className={classes.content}>
+            <Grid container style={{ alignItems: "center", height: "100%" }}>
+              <Tooltip title="Slots open" placement="bottom">
+                <GroupSvgIcon
+                  className={classes.menuIcons}
+                  style={{ cursor: "default" }}
+                  onClick={e => {
+                    console.log(e.target);
+                  }}
+                />
+              </Tooltip>
+              <div style={{ paddingLeft: "1.5rem" }}>
+                {this.state.form.openSlots} / {this.state.form.maxSlots}
+              </div>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={5} />
           <Grid item xs={4} className={classes.content}>
             <Button
               variant="raised"
