a7b4826fb4a7037c7e5a26ed89cc0f6f489d9bbc
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-05-25 13:38:55 -0800

    Feat: Responsive layout for main page

--- src/Content.js
+++ src/Content.js
@@ -1,23 +1,50 @@
-import React from "react";
-import Paper from "material-ui/Paper";
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
 import EventForm from "./EventForm";
 import dummyContent from "./demo/dummyData";
 
 const Content = () => (
-  <div className="mobile-desktop-container">
-    <Paper elevation={1}>
-      <EventForm
-        title={dummyContent.title}
-        description={dummyContent.content}
-        startDateTime={dummyContent.startDate}
-        endDateTime={dummyContent.endDate}
-        address={dummyContent.address}
-        openSlots={dummyContent.openSlots}
-        maxSlots={dummyContent.maxSlots}
-        courses={dummyContent.courses}
-      />
-    </Paper>
-  </div>
+  <ContentDesktopMobile>
+    <EventForm
+      title={dummyContent.title}
+      description={dummyContent.content}
+      startDateTime={dummyContent.startDate}
+      endDateTime={dummyContent.endDate}
+      address={dummyContent.address}
+      openSlots={dummyContent.openSlots}
+      maxSlots={dummyContent.maxSlots}
+      courses={dummyContent.courses}
+    />
+  </ContentDesktopMobile>
 );
+
+class ContentDesktopMobile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: window.innerWidth };
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", () => {
+      this.responsiveRender();
+    });
+  }
+
+  responsiveRender() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  render() {
+    if (this.state.width > 600) {
+      return (
+        <div className="mobile-desktop-container">
+          <Paper elevation={1}>{this.props.children}</Paper>
+        </div>
+      );
+    } else {
+      return this.props.children;
+    }
+  }
+}
 
 export default Content;
