a0adcc997a102bf3123ba08120ea9bece1b49cd5
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-06-01 14:55:41 -0800

    Test: Fixed unit tests for Course Select Component

--- src/components/CourseSelect.test.js
+++ src/components/CourseSelect.test.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom";
 import CourseSelect from "./CourseSelect";
 
 it("renders without crashing", () => {
+  const courses = [
+    { value: "class1", label: "Class 1" },
+    { value: "class2", label: "Class 2" },
+    { value: "class3", label: "Class 3" },
+    { value: "class4", label: "Class 4" }
+  ];
+
   const div = document.createElement("div");
-  ReactDOM.render(<CourseSelect />, div);
+  ReactDOM.render(<CourseSelect courses={courses} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
