d43aafa2f6a55e8a2cf6918048554fdd5a6c8292
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-06-01 14:38:39 -0800

    Test: Updating tests and test formatting
    Adding bare tests

--- src/components/CourseSelect.test.js
+++ src/components/CourseSelect.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import CourseSelect from "./CourseSelect";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<CourseSelect />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
