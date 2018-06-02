9e29c29aa90291d59af85c32d17a7232f2cc7762
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-06-01 16:04:31 -0800

    Chore: Stub for adding to calendar

---
+++ src/helpers/gCalendarAdd.js
@@ -0,0 +1,5 @@
+/* Calendar add documentation 
+ *  Ref: https://developers.google.com/calendar/v3/reference/events/insert
+ * https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjbxqTo1rPbAhUpiFQKHf5MApMQFggpMAA&url=https%3A%2F%2Fmedium.com%2F%40mattmazzola%2Freact-simple-auth-react-redux-oauth-2-0-de6ea9df0a63&usg=AOvVaw0yW_zRVBcdoNL0ifL_IrtV
+ * https://medium.com/front-end-hacking/use-github-oauth-as-your-sso-seamlessly-with-react-3e2e3b358fa1
+ */
