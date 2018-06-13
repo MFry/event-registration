d103c9d78f48f7caddd55a623fa06aeb0bcf00c9
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-06-12 18:06:51 -0800

    Feat: Codestub for event registration page

---
+++ src/Registration.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+
+const SimpleRegistrationForm = () => {
+  <div>
+    <Grid container>
+      <Grid item>Test</Grid>
+      <FormControl>
+          <InputLabel id="title"
+          </FormControl>
+    </Grid>
+  </div>;
+};
